deba0238deb604dd88c24e911a44eb3ec5ccfb72
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-01-28 21:46:43 -0800

    HLS-compatible lock, untested

--- lock.h
+++ lock.h
@@ -1,8 +1,10 @@
 #ifndef LOCK_H
 #define LOCK_H
 
-
-#ifdef __cplusplus
+#ifdef __SYNTHESIS__
+#include <ap_cint.h>
+typedef uint1 lock_t;
+#elif defined(__cplusplus)
 #include <atomic>
 typedef std::atomic_flag lock_t;
 #else
@@ -16,17 +18,25 @@ typedef atomic_flag lock_t;
 //! @brief Set the given lock to held
 static inline void lock_p(lock_t *lock) {
 	while (
-#ifdef __cplusplus
+#ifdef __SYNTHESIS__
+		*lock
+#elif defined(__cplusplus)
 		lock->test_and_set(std::memory_order_acquire)
 #else
 		atomic_flag_test_and_set(lock)
 #endif
-	);
+	) {
+#ifdef __SYNTHESIS__
+		*lock = 1;
+#endif
+	}
 }
 
 //! @brief Release the given lock
 static inline void lock_v(lock_t *lock) {
-#ifdef __cplusplus
+#ifdef __SYNTHESIS__
+	*lock = 0;
+#elif defined(__cplusplus)
 	lock->clear(std::memory_order_release);
 #else
 	atomic_flag_clear(lock);
