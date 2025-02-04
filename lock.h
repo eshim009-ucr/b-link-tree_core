1ab3b69cdad9a2b12344836a31c38c6944a2e87c
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-02-03 17:43:26 -0800

    Verified compilable lock under CSIM

--- lock.h
+++ lock.h
@@ -1,40 +1,62 @@
 #ifndef LOCK_H
 #define LOCK_H
 
-#ifdef __SYNTHESIS__
-#include <ap_cint.h>
-typedef uint1 lock_t;
-#elif defined(__cplusplus)
-#include <atomic>
-typedef std::atomic_flag lock_t;
+
+#define CSIM
+#if defined(CSIM)
+	#ifndef __cplusplus
+		#include <stdbool.h>
+	#endif
+	typedef bool lock_t;
+#elif defined(__SYNTHESIS__)
+	#include <ap_cint.h>
+	typedef uint1 lock_t;
+	#define TEST_AND_SET(lockptr) ((*lockptr) == 1 \
+	)
 #else
-#include <stdatomic.h>
-typedef atomic_flag lock_t;
+	#ifdef(__cplusplus)
+		#include <atomic>
+		typedef std::atomic_flag lock_t;
+		#define TEST_AND_SET(lockptr) ((lockptr)->test_and_set(std::memory_order_acquire))
+	#else
+		#include <stdatomic.h>
+		typedef atomic_flag lock_t;
+		#define TEST_AND_SET(lockptr) (atomic_flag_test_and_set(lockptr))
+	#endif
 #endif
 
-#include <stdbool.h>
+
+//! @brief Perform an atomic test-and-set operation on the given lock
+static inline bool test_and_set(lock_t *lock) {
+#if defined(CSIM) || defined(__SYNTHESIS__)
+	bool old = *lock;
+	*lock = true;
+	return old;
+#elif defined(__cplusplus)
+	return lock->test_and_set(std::memory_order_acquire);
+#else
+	return atomic_flag_test_and_set(lock);
+#endif
+}
+
+//! @brief Initialize a lock to its default (unset) value
+static inline void init_lock(lock_t *lock) {
+#if defined(CSIM) || defined(__SYNTHESIS__)
+	*lock = 0;
+#elif !defined(__cplusplus)
+	*lock = ATOMIC_FLAG_INIT;
+#endif
+}
 
 
 //! @brief Set the given lock to held
 static inline void lock_p(lock_t *lock) {
-	while (
-#ifdef __SYNTHESIS__
-		*lock
-#elif defined(__cplusplus)
-		lock->test_and_set(std::memory_order_acquire)
-#else
-		atomic_flag_test_and_set(lock)
-#endif
-	) {
-#ifdef __SYNTHESIS__
-		*lock = 1;
-#endif
-	}
+	while (test_and_set(lock));
 }
 
 //! @brief Release the given lock
 static inline void lock_v(lock_t *lock) {
-#ifdef __SYNTHESIS__
+#if defined(CSIM) || defined(__SYNTHESIS__)
 	*lock = 0;
 #elif defined(__cplusplus)
 	lock->clear(std::memory_order_release);
@@ -42,6 +64,7 @@ static inline void lock_v(lock_t *lock) {
 	atomic_flag_clear(lock);
 #endif
 }
+
 
 static inline bool lock_test(lock_t const *lock) {
 	//! @todo Do this better.
--- tree.c
+++ tree.c
@@ -5,7 +5,7 @@
 inline void init_tree(Tree *tree) {
 	memset(tree->memory, INVALID, MEM_SIZE*sizeof(Node));
 	for (uint_fast8_t i = 0; i < MEM_SIZE; i++) {
-		tree->memory[i].lock = (atomic_flag) ATOMIC_FLAG_INIT;
+		init_lock(&tree->memory[i].lock);
 	}
 	tree->root = 0;
 }
