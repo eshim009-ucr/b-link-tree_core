3d43f6d987658d834b01bfab2639b2f99c7a924d
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-04-04 17:58:56 -0700

    Insert now uses AddrNode structs

--- insert.c
+++ insert.c
@@ -31,42 +31,38 @@ inline static bkey_t max(Node *node) {
 
 //! @brief Allocate a new sibling node in an empty slot in main mameory
 //! @param[in]     root          Root of the tree the nodes reside in
-//! @param[in]     leaf_addr     The address of the node to split
 //! @param[in]     leaf          The node to split
-//! @param[inout]  parent_addr   The address of the parent of the node to split
-//! @param[inout]  parent        The parent of the node to split
-//! @param[out]    sibling_addr  The address of the split node's new sibling
 //! @param[out]    sibling       The contents of the split node's new sibling
 //!
 //! Acquires a lock on the sibling node
-static ErrorCode alloc_sibling(bptr_t *root,
-	bptr_t leaf_addr, Node *leaf,
-	bptr_t *sibling_addr, Node *sibling) {
-	const uint_fast8_t level = get_level(leaf_addr);
+static ErrorCode alloc_sibling(
+	bptr_t *root, AddrNode *leaf, AddrNode *sibling
+) {
+	const uint_fast8_t level = get_level(leaf->addr);
 
 	// Find an empty spot for the new leaf
-	for (*sibling_addr = level * MAX_NODES_PER_LEVEL;
-		*sibling_addr < (level+1) * MAX_NODES_PER_LEVEL;
-		++(*sibling_addr)) {
+	for (sibling->addr = level * MAX_NODES_PER_LEVEL;
+		sibling->addr < (level+1) * MAX_NODES_PER_LEVEL;
+		++sibling->addr) {
 		// Found an empty slot
-		if (leaf_addr != *sibling_addr && mem_read(*sibling_addr).keys[0] == INVALID) {
+		if (leaf->addr != sibling->addr && mem_read(sibling->addr).keys[0] == INVALID) {
 			break;
 		}
 	}
-	*sibling = mem_read_lock(*sibling_addr);
+	sibling->node = mem_read_lock(sibling->addr);
 	// If we didn't break, we didn't find an empty slot
-	if (*sibling_addr == (level+1) * MAX_NODES_PER_LEVEL) {
-		mem_unlock(*sibling_addr);
+	if (sibling->addr == (level+1) * MAX_NODES_PER_LEVEL) {
+		mem_unlock(sibling->addr);
 		return OUT_OF_MEMORY;
 	}
 	// Adjust next node pointers
-	sibling->next = leaf->next;
-	leaf->next = *sibling_addr;
+	sibling->node.next = leaf->node.next;
+	leaf->node.next = sibling->addr;
 	// Move half of old node's contents to new node
 	for (li_t i = 0; i < TREE_ORDER/2; ++i) {
-		sibling->keys[i] = leaf->keys[i + (TREE_ORDER/2)];
-		sibling->values[i] = leaf->values[i + (TREE_ORDER/2)];
-		leaf->keys[i + (TREE_ORDER/2)] = INVALID;
+		sibling->node.keys[i] = leaf->node.keys[i + (TREE_ORDER/2)];
+		sibling->node.values[i] = leaf->node.values[i + (TREE_ORDER/2)];
+		leaf->node.keys[i + (TREE_ORDER/2)] = INVALID;
 	}
 
 	return SUCCESS;
@@ -75,22 +71,17 @@ static ErrorCode alloc_sibling(bptr_t *root,
 
 //! @brief Assign an allocated sibling pair at the root leve of the tree
 //! @param[in]     root          Root of the tree the nodes reside in
-//! @param[in]     leaf_addr     The address of the node to split
 //! @param[in]     leaf          The node to split
-//! @param[inout]  parent_addr   The address of the parent of the node to split
 //! @param[inout]  parent        The parent of the node to split
-//! @param[out]    sibling_addr  The address of the split node's new sibling
 //! @param[out]    sibling       The contents of the split node's new sibling
 //! @return An error code representing the success or type of failure of the
 //!         operation
-static ErrorCode split_root(bptr_t *root,
-	bptr_t leaf_addr, Node *leaf,
-	bptr_t *parent_addr, Node *parent,
-	bptr_t *sibling_addr, Node *sibling
+static ErrorCode split_root(
+	bptr_t *root, AddrNode *leaf, AddrNode *parent, AddrNode *sibling
 ) {
 	// If this is the only node
 	// We need to create the first inner node
-	if (is_leaf(leaf_addr)) {
+	if (is_leaf(leaf->addr)) {
 		// Make a new root node
 		*root = MAX_LEAVES;
 	} else {
@@ -100,47 +91,42 @@ static ErrorCode split_root(bptr_t *root,
 			*root = *root + MAX_NODES_PER_LEVEL;
 		}
 	}
-	*parent_addr = *root;
-	*parent = mem_read_lock(*parent_addr);
-	init_node(parent);
-	parent->keys[0] = leaf->keys[DIV2CEIL(TREE_ORDER)-1];
-	parent->values[0].ptr = leaf_addr;
-	parent->keys[1] = sibling->keys[(TREE_ORDER/2)-1];
-	parent->values[1].ptr = *sibling_addr;
+	parent->addr = *root;
+	parent->node = mem_read_lock(parent->addr);
+	init_node(&parent->node);
+	parent->node.keys[0] = leaf->node.keys[DIV2CEIL(TREE_ORDER)-1];
+	parent->node.values[0].ptr = leaf->addr;
+	parent->node.keys[1] = sibling->node.keys[(TREE_ORDER/2)-1];
+	parent->node.values[1].ptr = sibling->addr;
 	return SUCCESS;
 }
 
 
 //! @brief Assign an allocated sibling pair below the root leve of the tree
 //! @param[in]     root          Root of the tree the nodes reside in
-//! @param[in]     leaf_addr     The address of the node to split
 //! @param[in]     leaf          The node to split
-//! @param[inout]  parent_addr   The address of the parent of the node to split
 //! @param[inout]  parent        The parent of the node to split
-//! @param[out]    sibling_addr  The address of the split node's new sibling
 //! @param[out]    sibling       The contents of the split node's new sibling
 //! @return An error code representing the success or type of failure of the
 //!         operation
-static ErrorCode split_nonroot(bptr_t *root,
-	bptr_t leaf_addr, Node *leaf,
-	bptr_t *parent_addr, Node *parent,
-	bptr_t *sibling_addr, Node *sibling
+static ErrorCode split_nonroot(
+	bptr_t *root, AddrNode *leaf, AddrNode *parent, AddrNode *sibling
 ) {
-	if (is_full(parent)) {
+	if (is_full(&parent->node)) {
 		return NOT_IMPLEMENTED;
 	} else {
 		for (li_t i = 0; i < TREE_ORDER; ++i) {
 			// Update key of old node
-			if (parent->values[i].ptr == leaf_addr) {
-				parent->keys[i] = leaf->keys[DIV2CEIL(TREE_ORDER)-1];
+			if (parent->node.values[i].ptr == leaf->addr) {
+				parent->node.keys[i] = leaf->node.keys[DIV2CEIL(TREE_ORDER)-1];
 				// Scoot over other nodes to fit in new node
 				for (li_t j = TREE_ORDER-1; j > i; --j) {
-					parent->keys[j] = parent->keys[j-1];
-					parent->values[j] = parent->values[j-1];
+					parent->node.keys[j] = parent->node.keys[j-1];
+					parent->node.values[j] = parent->node.values[j-1];
 				}
 				// Insert new node
-				parent->keys[i+1] = sibling->keys[(TREE_ORDER/2)-1];
-				parent->values[i+1].ptr = *sibling_addr;
+				parent->node.keys[i+1] = sibling->node.keys[(TREE_ORDER/2)-1];
+				parent->node.values[i+1].ptr = sibling->addr;
 				return SUCCESS;
 			}
 		}
@@ -151,24 +137,19 @@ static ErrorCode split_nonroot(bptr_t *root,
 
 //! @brief Split a node in the tree and return the affected nodes
 //! @param[in]     root          Root of the tree the nodes reside in
-//! @param[in]     leaf_addr     The address of the node to split
 //! @param[in]     leaf          The node to split
-//! @param[inout]  parent_addr   The address of the parent of the node to split
 //! @param[inout]  parent        The parent of the node to split
-//! @param[out]    sibling_addr  The address of the split node's new sibling
 //! @param[out]    sibling       The contents of the split node's new sibling
 //! @return An error code representing the success or type of failure of the
 //!         operation
-static ErrorCode split_node(bptr_t *root,
-	bptr_t leaf_addr, Node *leaf,
-	bptr_t *parent_addr, Node *parent,
-	bptr_t *sibling_addr, Node *sibling
+static ErrorCode split_node(
+	bptr_t *root, AddrNode *leaf, AddrNode *parent, AddrNode *sibling
 ) {
-	alloc_sibling(root, leaf_addr, leaf, sibling_addr, sibling);
-	if (*parent_addr == INVALID) {
-		return split_root(root, leaf_addr, leaf, parent_addr, parent, sibling_addr, sibling);
+	alloc_sibling(root, leaf, sibling);
+	if (parent->addr == INVALID) {
+		return split_root(root, leaf, parent, sibling);
 	} else {
-		return split_nonroot(root, leaf_addr, leaf, parent_addr, parent, sibling_addr, sibling);
+		return split_nonroot(root, leaf, parent, sibling);
 	}
 }
 
@@ -209,8 +190,7 @@ static ErrorCode insert_nonfull(Node *node, bkey_t key, bval_t value) {
 ErrorCode insert(bptr_t *root, bkey_t key, bval_t value) {
 	ErrorCode status;
 	li_t i_leaf;
-	Node leaf, parent, sibling;
-	bptr_t leaf_addr, parent_addr, sibling_addr;
+	AddrNode leaf, parent, sibling;
 	bptr_t lineage[MAX_LEVELS];
 
 	// Initialize lineage array
@@ -219,40 +199,40 @@ ErrorCode insert(bptr_t *root, bkey_t key, bval_t value) {
 	status = trace_lineage(*root, key, lineage);
 	if (status != SUCCESS) return status;
 	i_leaf = get_leaf_idx(lineage);
-	leaf_addr = lineage[i_leaf];
-	leaf = mem_read_lock(leaf_addr);
+	leaf.addr = lineage[i_leaf];
+	leaf.node = mem_read_lock(leaf.addr);
 	if (i_leaf > 0) {
-		parent_addr = lineage[i_leaf-1];
-		parent = mem_read_lock(parent_addr);
+		parent.addr = lineage[i_leaf-1];
+		parent.node = mem_read_lock(parent.addr);
 	} else {
-		parent_addr = INVALID;
+		parent.addr = INVALID;
 	}
 
 	// Search within the leaf node of the lineage for the key
-	if (is_full(&leaf)) {
+	if (is_full(&leaf.node)) {
 		// Try to split this node, exit on failure
-		status = split_node(root, leaf_addr, &leaf, &parent_addr, &parent, &sibling_addr, &sibling);
+		status = split_node(root, &leaf, &parent, &sibling);
 		if (status != SUCCESS) {
-			mem_unlock(leaf_addr);
-			mem_unlock(sibling_addr);
-			mem_unlock(parent_addr);
+			mem_unlock(leaf.addr);
+			mem_unlock(sibling.addr);
+			mem_unlock(parent.addr);
 			return status;
 		}
 		// Insert the new data
-		if (key < max(&leaf)) {
-			status = insert_nonfull(&leaf, key, value);
+		if (key < max(&leaf.node)) {
+			status = insert_nonfull(&leaf.node, key, value);
 		} else {
-			status = insert_nonfull(&sibling, key, value);
+			status = insert_nonfull(&sibling.node, key, value);
 		}
-		mem_write_unlock(sibling_addr, sibling);
-		mem_write_unlock(parent_addr, parent);
+		mem_write_unlock(sibling.addr, sibling.node);
+		mem_write_unlock(parent.addr, parent.node);
 		if (status != SUCCESS) return status;
 	} else {
-		status = insert_nonfull(&leaf, key, value);
-		if (parent_addr != INVALID) mem_unlock(parent_addr);
+		status = insert_nonfull(&leaf.node, key, value);
+		if (parent.addr != INVALID) mem_unlock(parent.addr);
 		if (status != SUCCESS) return status;
 	}
 
-	mem_write_unlock(leaf_addr, leaf);
+	mem_write_unlock(leaf.addr, leaf.node);
 	return SUCCESS;
 }
