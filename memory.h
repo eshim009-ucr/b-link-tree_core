2bf219383d986e77f75e9a02cc067ff9403e0c40
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-04-12 22:15:24 -0700

    Started integrating HLS memory streams to memory API

--- memory.h
+++ memory.h
@@ -8,22 +8,33 @@
 typedef struct Node Node;
 typedef struct AddrNode AddrNode;
 
+#ifdef __cplusplus
+#include "../memgr.hpp"
+#include <hls_stream.h>
+typedef hls::stream<mread_req_t> mread_req_stream_t;
+typedef hls::stream<mread_resp_t> mread_resp_stream_t;
+typedef hls::stream<mwrite_t> mwrite_stream_t;
+#else
+typedef void mread_req_stream_t;
+typedef void mread_resp_stream_t;
+typedef void mwrite_stream_t;
+#endif
 
 //! @brief Read a node from memory without grabbing any locks
-Node mem_read(bptr_t address);
+Node mem_read(bptr_t address, mread_req_stream_t *req_stream, mread_resp_stream_t *resp_stream);
 
 //! @brief Read a node from memory, locking it in memory
-Node mem_read_lock(bptr_t address);
+Node mem_read_lock(bptr_t address, mread_req_stream_t *req_stream, mread_resp_stream_t *resp_stream);
 
 //! @brief Write a node to memory and unlock it
-void mem_write_unlock(AddrNode *node);
+void mem_write_unlock(AddrNode *node, mwrite_stream_t *req_stream);
 
 //! @brief Unlock a node in memory
 //!
 //! Used when a node has been read and locked, but should be unlocked without
 //! modification. For example, if an error has been detected before a
 //! modification operation completes.
-void mem_unlock(bptr_t address);
+void mem_unlock(bptr_t address, mwrite_stream_t *req_stream);
 
 //! @brief Reset memory to a slate of blank nodes
 //!
